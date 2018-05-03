24033636a563f146b4436b8aa53f915cabeeb4b2
RyanJabro <league@WTS-iMac-11.attlocal.net>
2018-05-02 20:00:00 -0700

    Roller Coaster and Voting Booth

---
+++ src/methods/RollerCoaster.java
@@ -0,0 +1,39 @@
+package methods;
+
+import javax.swing.JOptionPane;
+
+public class RollerCoaster {
+public static void main(String[] args) {
+
+	
+	
+	
+	
+	
+	
+	String height = JOptionPane.showInputDialog(null, "How tall are you (in inches)?");
+	
+	
+	
+int biggness =Integer.parseInt(height);
+
+if(biggness>=47){JOptionPane.showMessageDialog(null, "YOU CAN RIDE BECAUSE YOU ARE TALL");
+}
+
+
+	
+else{JOptionPane.showMessageDialog(null, "YOU CAN'T RIDE SHORTY, HAHAHAHAHAHA");
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
---
+++ src/methods/Voting_Booth.java
@@ -0,0 +1,64 @@
+package methods;
+
+import javax.swing.JOptionPane;
+
+public class Voting_Booth {
+
+	public static void main(String[] args) {
+		
+		
+		
+		String height = JOptionPane.showInputDialog(null, "How old are you?");
+		
+		
+		
+		int biggness =Integer.parseInt(height);
+
+		if(biggness>=17){JOptionPane.showInputDialog(null, "Who do you want to be president?");
+		}
+
+
+			
+		else{JOptionPane.showMessageDialog(null, "NOBODY CARES WHAT YOU THINK!!!");
+		}	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
